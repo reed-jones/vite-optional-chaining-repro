6e5ab4fe1d1d68ceb3852b0020838accc07eda86
Reed Jones <reed.jones@pderas.com>
2020-06-17 21:47:57 -0600

    make it fail

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
+const failed = {}
+console.warn(failed?.will?.not?.have?.an?.issue?.in?.dev ?? 'but will fail in production')
 
 createApp(App).mount('#app')
